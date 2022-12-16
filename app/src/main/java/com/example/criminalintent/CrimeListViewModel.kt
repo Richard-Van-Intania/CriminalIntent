9a843fdd0d4e5293be2598b022f61f4aec89a45e
Richard <85865743+Richard-Van-Intania@users.noreply.github.com>
2022-12-16 16:02:22 +0700

    impl StateFlow

--- app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListViewModel.kt
@@ -3,14 +3,19 @@ package com.example.criminalintent
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import java.util.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 private const val TAG = "CrimeListViewModel"
 
 class CrimeListViewModel : ViewModel() {
   private val crimeRepository = CrimeRepository.get()
-  val crimes = crimeRepository.getCrimes()
+  private val _crimes: MutableStateFlow<List<Crime>> = MutableStateFlow(emptyList())
+  val crimes: StateFlow<List<Crime>>
+    get() = _crimes.asStateFlow()
   init {
-    viewModelScope.launch {}
+    viewModelScope.launch { crimeRepository.getCrimes().collect { _crimes.value = it } }
   }
 }

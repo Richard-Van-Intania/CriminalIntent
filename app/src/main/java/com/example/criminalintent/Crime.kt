578702fd74d3728115c709464fd819f79ec9e480
Richard <85865743+Richard-Van-Intania@users.noreply.github.com>
2022-12-16 12:15:31 +0700

    commit jaa

--- app/src/main/java/com/example/criminalintent/Crime.kt
+++ app/src/main/java/com/example/criminalintent/Crime.kt
@@ -1,5 +1,13 @@
 package com.example.criminalintent
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.*
 
-data class Crime(val id: UUID, val title: String, val date: Date, val isSolved: Boolean) {}
+@Entity
+data class Crime(
+    @PrimaryKey val id: UUID,
+    val title: String,
+    val date: Date,
+    val isSolved: Boolean
+) {}
--- app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeListFragment.kt
@@ -1,16 +1,16 @@
 package com.example.criminalintent
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.criminalintent.databinding.FragmentCrimeListBinding
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
 private const val TAG = "CrimeListFragment"
@@ -18,18 +18,11 @@ private const val TAG = "CrimeListFragment"
 class CrimeListFragment : Fragment() {
 
   private val crimeListViewModel: CrimeListViewModel by viewModels()
-
-  private var job: Job? = null
-
   private var _binding: FragmentCrimeListBinding? = null
   private val binding
     get() =
         checkNotNull(_binding) { "Cannot access binding because it is null. Is the view visible?" }
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
-  }
   override fun onCreateView(
       inflater: LayoutInflater,
       container: ViewGroup?,
@@ -37,29 +30,20 @@ class CrimeListFragment : Fragment() {
   ): View {
     _binding = FragmentCrimeListBinding.inflate(inflater, container, false)
     binding.crimeRecyclerView.layoutManager = LinearLayoutManager(context)
-    //    val crimes = crimeListViewModel.crimes
-    //    val adapter = CrimeListAdapter(crimes)
-    binding.crimeRecyclerView.adapter = CrimeListAdapter(crimeListViewModel.crimes)
     return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    viewLifecycleOwner.lifecycleScope.launch {
+      viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+        binding.crimeRecyclerView.adapter = CrimeListAdapter(crimeListViewModel.loadCrimes())
+      }
+    }
   }
 
   override fun onDestroyView() {
     super.onDestroyView()
     _binding = null
-  }
-
-  override fun onStart() {
-    super.onStart()
-
-    job =
-        viewLifecycleOwner.lifecycleScope.launch {
-          val crimes = crimeListViewModel.loadCrimes()
-          binding.crimeRecyclerView.adapter = CrimeListAdapter(crimes)
-        }
-  }
-
-  override fun onStop() {
-    super.onStop()
-    job?.cancel()
   }
 }

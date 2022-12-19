3b37482aa3a455d090f8245c747e9c6e314aad8b
Richard <85865743+Richard-Van-Intania@users.noreply.github.com>
2022-12-19 11:19:48 +0700

    up to updating database

--- app/src/main/java/com/example/criminalintent/CrimeDetailFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeDetailFragment.kt
@@ -1,32 +1,34 @@
 package com.example.criminalintent
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.navArgs
 import com.example.criminalintent.databinding.FragmentCrimeDetailBinding
 import java.util.*
+import kotlinx.coroutines.launch
 
 private const val TAG = "CrimeDetailFragment"
 
 class CrimeDetailFragment : Fragment() {
 
-  private lateinit var crime: Crime
   private val args: CrimeDetailFragmentArgs by navArgs()
+
+  private val crimeDetailViewModel: CrimeDetailViewModel by viewModels {
+    CrimeDetailViewModelFactory(args.crimeId)
+  }
+
   private var _binding: FragmentCrimeDetailBinding? = null
   private val binding
     get() =
         checkNotNull(_binding) { "Cannot access binding because it is null. Is the view visible?" }
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    crime = Crime(id = UUID.randomUUID(), title = "", date = Date(), isSolved = false)
-    Log.d(TAG, "The crime ID is: ${args.crimeId}")
-  }
 
   override fun onCreateView(
       inflater: LayoutInflater,
@@ -41,15 +43,18 @@ class CrimeDetailFragment : Fragment() {
     super.onViewCreated(view, savedInstanceState)
     binding.apply {
       crimeTitle.doOnTextChanged { text, _, _, _ ->
-        crime = crime.copy(title = text.toString().trim())
+        crimeDetailViewModel.updateCrime { oldCrime -> oldCrime.copy(title = text.toString()) }
       }
 
-      crimeDate.apply {
-        text = crime.date.toString()
-        isEnabled = false
-      }
+      crimeDate.apply { isEnabled = false }
+
       crimeSolved.setOnCheckedChangeListener { _, isChecked ->
-        crime = crime.copy(isSolved = isChecked)
+        crimeDetailViewModel.updateCrime { oldCrime -> oldCrime.copy(isSolved = isChecked) }
+      }
+    }
+    viewLifecycleOwner.lifecycleScope.launch {
+      viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+        crimeDetailViewModel.crime.collect { crime -> crime?.let { updateUi(it) } }
       }
     }
   }
@@ -57,5 +62,15 @@ class CrimeDetailFragment : Fragment() {
   override fun onDestroyView() {
     super.onDestroyView()
     _binding = null
+  }
+
+  private fun updateUi(crime: Crime) {
+    binding.apply {
+      if (crimeTitle.text.toString() != crime.title) {
+        crimeTitle.setText(crime.title)
+      }
+      crimeDate.text = crime.date.toString()
+      crimeSolved.isChecked = crime.isSolved
+    }
   }
 }
